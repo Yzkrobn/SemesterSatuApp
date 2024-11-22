1ea05b397155fcdd725764e5059d5c3302ee8a1b
Yzkrobn <oktavianbagasnugroho@gmailcom>
2024-11-22 14:12:48 +0700

    Pesan commit pertama

--- app/src/main/java/com/example/tugassemestersatuapp/CardViewProvileActivity.kt
+++ app/src/main/java/com/example/tugassemestersatuapp/CardViewProvileActivity.kt
@@ -1,20 +1,46 @@
 package com.example.tugassemestersatuapp
 
+import android.annotation.SuppressLint
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CardViewProvileActivity : AppCompatActivity() {
+    private lateinit var TVSelamat: TextView
+    private lateinit var TVWaktu: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_card_view_provile)
+        init()
+        pewaktu()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun init(){
+        TVSelamat = findViewById(R.id.TVSelamat)
+        TVWaktu = findViewById(R.id.TVWaktu)
+    }
+    @SuppressLint("SimpleDateFormat")
+    private fun pewaktu() {
+        val jam = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val hth = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+        val result = when (jam) {
+            in 0..11 -> "Selamat Pagi"
+            in 12..15 -> "Selamat Siang"
+            in 16..18 -> "Selamat Sore"
+            else -> "Selamat Malam"
+        }
+        TVSelamat.text = result
+        TVWaktu.text = hth.format(Calendar.getInstance().time)
     }
 }
--- app/src/main/java/com/example/tugassemestersatuapp/KalkulatorSederhanaActivity.kt
+++ app/src/main/java/com/example/tugassemestersatuapp/KalkulatorSederhanaActivity.kt
@@ -1,20 +1,58 @@
 package com.example.tugassemestersatuapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class KalkulatorSederhanaActivity : AppCompatActivity() {
+    lateinit var angka1:EditText
+    lateinit var angka2:EditText
+    lateinit var operasi: Spinner
+    lateinit var hitung: Button
+
+    var bil1 : Double = 0.0
+    var bil2 : Double = 0.0
+    var hasil : Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_kalkulator_sederhana2)
+        setContentView(R.layout.activity_kalkulator_sederhana)
+        init()
+        hitung.setOnClickListener(){
+            hitung()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun init(){
+        angka1 = findViewById(R.id.EtBil1)
+        angka2 = findViewById(R.id.EtBil2)
+        operasi = findViewById(R.id.SOprasiAritmatika)
+        hitung = findViewById(R.id.BtHitung)
+    }
+    fun hitung(){
+        bil1 = angka1.text.toString().toDoubleOrNull() ?: 0.0
+        bil2 = angka2.text.toString().toDoubleOrNull() ?: 0.0
+
+        if (bil2==0){
+
+        }
+        hasil = when(operasi.selectedItem){
+            0->bil1+bil2
+            1->bil1+bil2
+            2->bil1+bil2
+            3->bil1+bil2
+        }
+
+
     }
 }
